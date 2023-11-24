8d7ba5bd0c4d5143134bb938e6501d9d17803c85
Matthias <aussieevil@hotmail.com>
2023-11-24 15:20:06 -0800

    Engine almost ready for rehearsal
    Also added documentation to Sequence_Generator
    
    TODO: Add basic capability to the block class and test!

--- addons/fbm3s/fbm3s_engine.gd
+++ addons/fbm3s/fbm3s_engine.gd
@@ -28,7 +28,7 @@ enum HardDropBehavior {
 }
 ## How the lock timer behaves once the triad hits the bottom.
 ##
-## For all options besides INSTANT_LOCK, the timer is paused if the falling triad has empty space below it.
+## For all options besides INSTANT_LOCK, the timer resets (or is paused if ENTRY_RESET) if the falling triad has empty space below it.
 enum LockTimerBehavior {
 	INSTANT_LOCK, ## The triad instantly locks when hitting bottom.
 	ENTRY_RESET, ## The lock timer resets when a new triad enters the playfield.
@@ -66,13 +66,13 @@ const DEFAULT_BLOCK_SCENE = "res://addons/fbm3s/fbm3s_block.tscn"
 @export_range(4,8) var tile_kinds = 6
 ## If [code]true[/code], will check for matches diagonally.
 @export var allow_diagonal_matches := true
-## The row where new triads spawn.
+## The row where new triads spawn. See [enum CursorSpawnRow].
 @export var triad_entry_row = CursorSpawnRow.ON_TOP_ROW
-## What counts as a top out.
+## What counts as a top out. See [enum TopOutMode].
 @export var top_out_when = TopOutMode.ANY_OUTSIDE
 @export_subgroup("Sequence Generation")
 ## The random sequence generator to use. 
-## If empty, the default SequenceGenerator will be used at runtime.
+## If empty, the default [SequenceGenerator] will be used at runtime.
 @export var sequence_generator: SequenceGenerator
 ## How many triads are shown in the next queue.
 @export_range(1,4,1,"suffix:triads") var next_queue_length = 1
@@ -80,9 +80,9 @@ const DEFAULT_BLOCK_SCENE = "res://addons/fbm3s/fbm3s_block.tscn"
 ## Whether or not soft dropping (temporarily doubling gravity while the input is held)
 ## is allowed.
 @export var use_soft_drop := true
-## The behavior of hard drops.
+## The behavior of hard drops. See [enum HardDropBehavior].
 @export var hard_drop_style := HardDropBehavior.HARD_DROP
-## How the lockdown timer behaves.
+## How the lockdown timer behaves. See [enum LockTimerBehavior].
 @export var lockdown_style := LockTimerBehavior.GRAV_RESET
 @export_subgroup("Timers")
 ## Time it takes for the triad to drop down a single row from gravity.
@@ -96,6 +96,32 @@ const DEFAULT_BLOCK_SCENE = "res://addons/fbm3s/fbm3s_block.tscn"
 ## The delay between phases.
 @export_range(0.05, 0.1, 0.01, "suffix:s") var interval_time = 0.07
 
+var cursor_location: Vector2i:
+	get: return _cursor_location
+	set(v):	pass
+var current_triad: Array:
+	get: return _current_triad
+	set(v): pass
+var next_queue: Array:
+	get: return _next_queue
+	set(v): pass
+var grav_time_left: float:
+	get: return _grav_timer.time_left
+	set(v): pass
+var lock_time_left: float:
+	get: return _lock_timer.time_left
+	set(v): pass
+var flash_time_left: float:
+	get: return _flash_timer.time_left
+	set(v): pass
+var interval_time_left: float:
+	get: return _interval_timer.time_left
+	set(v): pass
+var game_active: bool:
+	get: return _game_active
+	set(v): pass
+
+var _game_active := false
 var _block_scene: PackedScene
 var _grav_timer = Timer.new()
 var _lock_timer = Timer.new()
@@ -111,6 +137,7 @@ var _is_soft_dropping := false
 var _before_triad_entry: Callable
 var _before_match_check: Callable
 var _before_flash: Callable
+var _combo: int = 0
 
 func _ready():
 	#sanity check time!
@@ -140,6 +167,7 @@ func _ready():
 	if _block_matrix == null:
 		return
 	# All the dependency checks passed? Good, let's go on
+	connect("top_out", Callable(self, "_topped_out"))
 	add_child(_cursor)
 	_set_up_timers()
 	_reset_next_queue()
@@ -151,39 +179,41 @@ func get_block_texture(which: int) -> AtlasTexture:
 	result.region = Rect2(which * tile_size, 0, tile_size, tile_size)
 	return result
 
-func get_current_triad() -> Array:
-	return _current_triad
-
-func get_next_queue() -> Array:
-	return _next_queue
-
-func get_timers() -> Dictionary:
-	var result = {
-		"grav": _grav_timer.time_left,
-		"lock": _lock_timer.time_left,
-		"flash": _flash_timer.time_left,
-		"interval": _interval_timer.time_left,
-	}
-	return result
-
 func slide_cursor(what_dir: Direction) -> bool:
+	if _cursor_location.x + what_dir in range(field_size.x):
+		if _block_matrix[_cursor_location.x + what_dir][_cursor_location.y] == null:
+			_cursor_location.x += what_dir
+			if lockdown_style == LockTimerBehavior.MOVE_RESET:
+				_lock_timer.start()
+			_update_cursor()
+			return true
 	return false
 
 func rotate_triad_down():
 	_current_triad.push_front(_current_triad.pop_back())
+	if lockdown_style == LockTimerBehavior.MOVE_RESET:
+		_lock_timer.start()
 	_update_cursor()
 
 func rotate_triad_up():
 	_current_triad.push_back(_current_triad.pop_front())
+	if lockdown_style == LockTimerBehavior.MOVE_RESET:
+		_lock_timer.start()
 	_update_cursor()
 
 func start_soft_drop():
 	if use_soft_drop:
 		_is_soft_dropping = true
+		_drop_cursor()
 
 func stop_soft_drop():
 	_is_soft_dropping = false
+	_grav_timer.start(_grav_timer.time_left * 2.0)
 
+## Performs a hard drop, moving the triad to the ground instantly.
+## The behavior of the triad depends on [member hard_drop_style].
+##
+## Returns the number of rows dropped.
 func hard_drop() -> int:
 	if hard_drop_style != HardDropBehavior.NONE:
 		return 0
@@ -193,6 +223,7 @@ func hard_drop() -> int:
 		var target = lowest - 1 if (lowest >= 0) else field_size.y - 1
 		var result = target - _cursor_location.y
 		_cursor_location.y = target
+		_update_cursor()
 		match hard_drop_style:
 			HardDropBehavior.FIRM_DROP:
 				_activate_lockdown_timer()
@@ -201,8 +232,8 @@ func hard_drop() -> int:
 		return result
 
 func start_game():
+	_game_active = true
 	_spawn_triad()
-	_grav_timer.start()
 
 func reset_matrix(reset_queue := false):
 	_block_matrix = _set_up_array()
@@ -210,8 +241,11 @@ func reset_matrix(reset_queue := false):
 	if reset_queue:
 		_reset_next_queue()
 
-func put_block_at(which: int, where: Vector2i, clobber = false) -> bool:
-	if where < field_size:
+func is_valid_coordinate(where: Vector2i) -> bool:
+	return where < field_size and where > Vector2i(0,0)
+
+func put_block_at(which: int, where: Vector2i, clobber := false) -> bool:
+	if is_valid_coordinate(where):
 		if _block_matrix[where.x][where.y] == null:
 			var new_block = _block_scene.instantiate() as Fbm3sBlock
 			_block_matrix[where.x][where.y] = new_block
@@ -256,7 +290,6 @@ func _set_up_timers():
 	_lock_timer.wait_time = lock_time
 	add_child(_lock_timer)
 	
-	_flash_timer.connect("timeout", Callable(self, "_pop_blocks"))
 	_flash_timer.one_shot = true
 	_flash_timer.wait_time = flash_time
 	add_child(_flash_timer)
@@ -275,6 +308,7 @@ func _reset_next_queue():
 		_next_queue.push_front(sequence_generator.get_sequence(3))
 
 func _spawn_triad():
+	_combo = 0
 	var middle = floor(field_size.x / 2.0)
 	if _block_matrix[middle][0] == null:
 		if _before_triad_entry.is_valid():
@@ -284,8 +318,9 @@ func _spawn_triad():
 		_cursor_location.x = middle
 		_update_cursor()
 		_cursor.show()
+		_grav_timer.start()
 	else:
-		emit_signal("top_out")
+		top_out.emit()
 
 func _advance_triad():
 	_current_triad = _next_queue.pop_back()
@@ -297,48 +332,130 @@ func _update_cursor():
 	_cursor.bottom_sprite.texture = get_block_texture(_current_triad[2])
 	_cursor.position = _playfield.tile_to_pixel(_cursor_location)
 
+func _drop_cursor():
+	_cursor_location.y += 1
+	_update_cursor()
+	match lockdown_style:
+		LockTimerBehavior.ENTRY_RESET:
+			_lock_timer.paused = true
+		LockTimerBehavior.GRAV_RESET, LockTimerBehavior.MOVE_RESET:
+			_lock_timer.stop()
+	if _block_matrix[_cursor_location.x][_cursor_location.y + 1] == null \
+	  and _cursor_location.y + 1 < field_size.y:
+		if _is_soft_dropping:
+			soft_drop_row.emit()
+			_grav_timer.start(gravity_time / 2.0)
+		else:
+			_grav_timer.start(gravity_time)
+	elif lockdown_style != LockTimerBehavior.INSTANT_LOCK:
+		_activate_lockdown_timer()
+	else:
+		_lock_down()
+
 func _activate_lockdown_timer():
-	pass
+	_is_soft_dropping = false
+	if _lock_timer.paused == true:
+		_lock_timer.paused = false
+	else:
+		_lock_timer.start()
 
 func _lock_down():
-	pass
+	_cursor.hide()
+	if _cursor_location.y >= 0:
+		var all_placed = true
+		for i in _current_triad.size():
+			var loc = _cursor_location
+			loc.y -= 2 - i
+			all_placed = all_placed and put_block_at(_current_triad[i],loc)
+		if top_out_when == TopOutMode.ANY_OUTSIDE and not all_placed:
+			top_out.emit()
+		else:
+			if _before_match_check.is_valid():
+				_before_match_check.call()
+			_combo_check()
+	else:
+		top_out.emit()
 
-func _check_for_matches():
-	for i in field_size.x:
-		for j in field_size.y:
+func _combo_check():
+	var matches = _check_for_matches()
+	while not matches.is_empty():
+		_combo += 1
+		match_made.emit(matches.size(), _combo)
+		if _before_flash.is_valid():
+			_before_flash.call()
+		get_tree().call_group("matched", "flash")
+		await _flash_timer.timeout
+		#if the block flash doesn't include queue_free, this will clean em up
+		get_tree().call_group("matched", "queue_free")
+		#and if these blocks are still awaiting "freedom," let's pop em off the matrix:
+		for i in matches:
+			_block_matrix[i.x][i.y] = null
+		_cascade_blocks()
+		matches = _check_for_matches()
+	if _combo > 0:
+		combo_ended.emit()
+	_interval_timer.start()
+
+func _check_for_matches() -> Array[Vector2i]:
+	var result: Array[Vector2i] = []
+	for i in range(1, field_size.x - 1):
+		for j in range(1, field_size.y - 1):
 			if _block_matrix[i][j] != null:
-				var _not_on_horiz_edge = i > 0 and i < (field_size.x - 1)
-				var _not_on_vert_edge = j > 0 and j < (field_size.y - 1)
 				var current_kind = _block_matrix[i][j].kind
-				if _not_on_horiz_edge:
-					if _block_matrix[i-1][j] != null and _block_matrix[i+1][j] != null:
-						if _block_matrix[i-1][j].kind == current_kind \
-						and _block_matrix[i+1][j].kind == current_kind:
-							_block_matrix[i-1][j].add_to_group("matched")
-							_block_matrix[i][j].add_to_group("matched")
-							_block_matrix[i+1][j].add_to_group("matched")
+				#check horizontally
+				_check_and_mark_match(Vector2i(i - 1, j), \
+				  Vector2i(i, j), Vector2i(i + 1, j), current_kind, result)
+				#check vertically
+				_check_and_mark_match(Vector2i(i, j - 1), \
+				  Vector2i(i, j), Vector2i(i, j + 1), current_kind, result)
 
-				if _not_on_vert_edge:
-					if _block_matrix[i][j-1] != null and _block_matrix[i][j+1] != null:
-						if _block_matrix[i][j-1].kind == current_kind \
-						and _block_matrix[i][j+1].kind == current_kind:
-							_block_matrix[i][j-1].add_to_group("matched")
-							_block_matrix[i][j].add_to_group("matched")
-							_block_matrix[i][j+1].add_to_group("matched")
+				if allow_diagonal_matches:
+					_check_and_mark_match(Vector2i(i - 1, j - 1), \
+					  Vector2i(i, j), Vector2i(i + 1, j + 1), current_kind, result)
+					_check_and_mark_match(Vector2i(i + 1, j - 1), \
+					  Vector2i(i, j), Vector2i(i - 1, j + 1), current_kind, result)
 
-				if _not_on_horiz_edge and _not_on_vert_edge and allow_diagonal_matches:
-					if _block_matrix[i-1][j-1] != null && _block_matrix[i+1][j+1] != null:
-						if _block_matrix[i-1][j-1].kind == current_kind \
-						  and _block_matrix[i+1][j+1].kind == current_kind:
-							_block_matrix[i-1][j-1].add_to_group("matched")
-							_block_matrix[i][j].add_to_group("matched")
-							_block_matrix[i+1][j+1].add_to_group("matched")
-					if _block_matrix[i+1][j-1] != null && _block_matrix[i-1][j+1] != null:
-						if _block_matrix[i+1][j-1].kind == current_kind \
-						  and _block_matrix[i-1][j+1].kind == current_kind:
-							_block_matrix[i+1][j-1].add_to_group("matched")
-							_block_matrix[i][j].add_to_group("matched")
-							_block_matrix[i-1][j+1].add_to_group("matched")
+	return result
+
+func _check_and_mark_match(a: Vector2i, b:Vector2i, c:Vector2i, to_match, list):
+	if _block_matrix[a.x][a.y] != null and _block_matrix[c.x][c.y] != null:
+		if _block_matrix[a.x][a.y].kind == to_match and _block_matrix[a.x][a.y].kind == to_match:
+			_mark_matched(a, list)
+			_mark_matched(b, list)
+			_mark_matched(c, list)
+
+func _mark_matched(loc: Vector2i, list: Array[Vector2i]):
+	_block_matrix[loc.x][loc.y].add_to_group("matched")
+	if loc not in list:
+		list.append(loc)
+
+func _cascade_blocks():
+	for col in field_size.x:
+		var lowest_space = -1
+		var block_bottom = -1
+		var block_top = -1
+		var move_made = true
+		var occupied: Array[bool] = []
+		occupied.resize(field_size.y)
+		occupied.fill(false)
+		for row in field_size.y:
+			occupied[row] = false if _block_matrix[col][row] == null else true
+		while move_made:
+			move_made = false
+			lowest_space = occupied.rfind(false)
+			block_bottom = occupied.rfind(true, lowest_space)
+			if block_bottom >= 0:
+				var to_move = lowest_space - block_bottom
+				move_made = true
+				block_top=(occupied.rfind(false, block_bottom))+1
+				for i in range(block_bottom, block_top, -1):
+					_block_matrix[col][i+to_move] = _block_matrix[col][i]
+					_block_matrix[col][i] = null
+		for row in field_size.y:
+			if _block_matrix[col][row] != null:
+				_block_matrix[col][row].move_to(_playfield.tile_to_pixel(Vector2(col, row)))
+
+func _topped_out(): _game_active = false
 
 class Cursor extends Node2D:
 	var top_sprite = Sprite2D.new()
--- addons/fbm3s/sequence_generators/sequence_generator.gd
+++ addons/fbm3s/sequence_generators/sequence_generator.gd
@@ -1,8 +1,14 @@
 class_name SequenceGenerator
 extends Resource
+## A basic pure random sequence generator.
+##
+## This sequence generator does not hold a sequence or history. 
+## Instead, it just generates an array of random numbers.
 
+## How many different kinds can be generated.
 var kinds_count: int = 1
 
+## Returns a random array of numbers.
 func get_sequence(length: int) -> Array[int]:
 	var result: Array[int] = []
 	assert(kinds_count > 0, "Don't use a non-positive kinds_count, doofus!")
